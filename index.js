95a6d2a2166fcf0c01bac75a54a4fb688f97d096
Harsh Mishra <mishraharsh.2803@gmail.com>
2026-03-02 19:50:54 +0530

    admin endpoint

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose");
 const JWT = require("jsonwebtoken");
 const {userRouter} = require("./route/user");
 const {courseRouter} = require("./route/course");
+const {adminRouter}  = require("./route/admin")
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/v1/user",userRouter);
+app.use("/api/v1/admin",userRouter);
 app.use("/api/v1/course",courseRouter);
 
 app.listen(3000);
