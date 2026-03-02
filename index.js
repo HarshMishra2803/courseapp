bac8f1112bd91f04f951b1069b48f8602e21fe9b
Harsh Mishra <mishraharsh.2803@gmail.com>
2026-03-02 19:05:45 +0530

    add skeleton routes

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-s
+const express = require("express");
+const mongoose = require("mongoose");
+const JWT = require("jsonwebtoken");
+
+const app = express();
+app.use(express);
+
+app.post("/user/singup",(req,res)=>{
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+app.post("/user/singin",(req,res)=>{
+    res.json({
+        message:"signin endpoint"
+    })
+})
+
+app.get("/user/purchases",(req,res)=>{
+    res.json({
+        message:"purchases endpoint"
+    })
+})
+
+app.get("/course/purchase",(req,res)=>{
+    res.json({
+        message:"purchases endpoint"
+    })
+})
+
+app.get("/courses",(req,res)=>{
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+app.listen(3000);
